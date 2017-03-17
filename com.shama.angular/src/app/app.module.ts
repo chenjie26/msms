5863b53eeeb966160d4c06433a5c795e9a887f78
chenjie <598384024@qq.com>
2017-03-17 19:34:20 +0800

    init

--- com.shama.angular/src/app/app.module.ts
+++ com.shama.angular/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component, OnInit} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {BaiduMap} from 'angular2-baidu-map/src';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
+    BaiduMap,
     AppComponent
   ],
   imports: [
