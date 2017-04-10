1c106b648d1c42549437b4ec185b73ebde142695
DESKTOP-KFMQQN8\jackson <ji.chen26cs@gmail.com>
2017-04-11 03:45:51 +0800

    init

--- angular/app/service/service.js
+++ angular/app/service/service.js
@@ -42,19 +42,19 @@ angular.module('myApp.service', ['ui.router', 'oc.lazyLoad', 'myApp.services'])
         angular.forEach($scope.services, function(service, index) {
             var showService = {service: service, image: '', key: ''};
             if (service.name == '桶装水') {
-                showService.image = '../images/main/s1.png';
+                showService.image = 'images/main/s1.png';
                 showService.key = '1';
             } else if (service.name == '客房维修') {
-                showService.image = '../images/main/s2.png';
+                showService.image = 'images/main/s2.png';
                 showService.key = '2';
             } else if (service.name == '洗衣机干洗') {
-                showService.image = '../images/main/s3.png';
+                showService.image = 'images/main/s3.png';
                 showService.key = '3';
             } else if (service.name == '床品') {
-                showService.image = '../images/main/s4.png';
+                showService.image = 'images/main/s4.png';
                 showService.key = '4';
             } else if (service.name == '婴儿照看') {
-                showService.image = '../images/main/s5.png';
+                showService.image = 'images/main/s5.png';
                 showService.key = '5';
             }
 
@@ -73,24 +73,24 @@ angular.module('myApp.service', ['ui.router', 'oc.lazyLoad', 'myApp.services'])
 
         angular.forEach($scope.showServices, function(showService, index) {
             if (showService.service.name == '桶装水') {
-                showService.image = '../images/main/s1.png';
+                showService.image = 'images/main/s1.png';
                 showService.key = '1';
             } else if (showService.service.name == '客房维修') {
-                showService.image = '../images/main/s2.png';
+                showService.image = 'images/main/s2.png';
                 showService.key = '2';
             } else if (showService.service.name == '洗衣机干洗') {
-                showService.image = '../images/main/s3.png';
+                showService.image = 'images/main/s3.png';
                 showService.key = '3';
             } else if (showService.service.name == '床品') {
-                showService.image = '../images/main/s4.png';
+                showService.image = 'images/main/s4.png';
                 showService.key = '4';
             } else if (showService.service.name == '婴儿照看') {
-                showService.image = '../images/main/s5.png';
+                showService.image = 'images/main/s5.png';
                 showService.key = '5';
             }
         });
 
-        service.image = '../images/main/s' + service.key + service.key +'.png';
+        service.image = 'images/main/s' + service.key + service.key +'.png';
     };
 
 
