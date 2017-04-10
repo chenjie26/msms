2b670419a87c3f5dd3d8cc8c925d75c0916169d2
DESKTOP-KFMQQN8\jackson <ji.chen26cs@gmail.com>
2017-04-11 00:00:39 +0800

    init

--- com.shama/routes/api.php
+++ com.shama/routes/api.php
@@ -22,3 +22,11 @@ $api->version('v1', function ($api) {
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::group(['middleware' => ['web', 'wechat.oauth']], function () {
+    Route::get('/user', function () {
+        $user = session('wechat.oauth_user'); // 拿到授权用户资料
+
+        dd($user);
+    });
+});
--- com.shama/routes/web.php
+++ com.shama/routes/web.php
@@ -28,13 +28,6 @@ Route::get('/', function () {
 $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', function ($api) {
     $api->group(['namespace' => 'App\Http\Controllers\Api'], function ($api) {
-        $api::group(['middleware' => ['web', 'wechat.oauth']], function ($api) {
-            $api->get('/wxUser', function () {
-                $user = session('wechat.oauth_user'); // 拿到授权用户资料
-
-                dd($user);
-            });
-        });
 //namespace声明路由组的命名空间，因为上面设置了"prefix"=>"api",所以以下路由都要加一个api前缀，比如请求/api/users_list才能访问到用户列表接口
         $api->group(['EnableCrossRequestMiddleware'], function ($api) {
 
